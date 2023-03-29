ff1216ddf37234541c52a92ed74232a15c71e32c
jan.sima <jan.sima@certicon.cz>
2023-03-29 20:59:37 +0200

    Jo toto chci commitovat

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,8 @@ import { DataStorageService } from '../shared/data-storage.service';
 export class HeaderComponent{
   constructor(private dataStorageService: DataStorageService){}
 
+  //Toto jsou pokusy s Gitem
+
   onSaveData(){
     this.dataStorageService.storeRecipes();
   }
